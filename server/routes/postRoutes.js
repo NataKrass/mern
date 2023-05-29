7522ecdff98cd14cda00922afc383cd32f7a4ca7
natal <natal@MacBook-Air-Natala.local>
2023-05-29 19:04:26 +0200

    initial commit

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+
+import Post from '../mongodb/models/post.js';
+
+dotenv.config();
+
+const router = express.Router();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+})
+
+export default router;
